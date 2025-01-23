16db58013920454f2cd731a75076c502c9407798
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2025-01-23 12:21:25 +0000

    add cmake file

---
+++ cmake/GenerateVersion.cmake
@@ -0,0 +1,19 @@
+find_package(Git)
+
+if(GIT_EXECUTABLE)
+    get_filename_component(WORKING_DIR ${SRC} DIRECTORY)
+    execute_process(
+            COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --always
+            WORKING_DIRECTORY ${WORKING_DIR}
+            OUTPUT_VARIABLE CHARON_VERSION
+            RESULT_VARIABLE ERROR_CODE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+endif()
+
+if(CHARON_VERSION STREQUAL "")
+    set(CHARON_VERSION 0.0.0-unknown)
+    message(WARNING "Failed to determine version from Git tags. Using default version \"${CHARON_VERSION}\".")
+endif()
+
+configure_file(${SRC} ${DST} @ONLY)
