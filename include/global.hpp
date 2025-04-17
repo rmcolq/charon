d4c122b8ccc20b41210d9482275d411902cfde7b
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2025-04-17 16:11:47 +0100

    add global file

---
+++ include/global.hpp
@@ -0,0 +1,8 @@
+#ifndef CHARON_GLOBAL_HPP
+#define CHARON_GLOBAL_HPP
+
+#include <vector>
+
+extern const std::vector<float> default_pos_data;
+extern const std::vector<float> default_neg_data;
+#endif //CHARON_GLOBAL_HPP
