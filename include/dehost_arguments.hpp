cc078f974b5e52b8018f70d3be155a94fbb5c83c
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2025-05-05 12:49:52 +0100

    realign argument printout

--- include/dehost_arguments.hpp
+++ include/dehost_arguments.hpp
@@ -47,31 +47,31 @@ struct DehostArguments {
         std::string ss;
 
         ss += "\n\nDehost Arguments:\n\n";
-        ss += "\tread_file:\t\t" + read_file.string() + "\n";
-        ss += "\tread_file2:\t\t" + read_file2.string() + "\n";
-        ss += "\tdb:\t\t\t" + db + "\n\n";
+        ss += "\tread_file:\t\t\t" + read_file.string() + "\n";
+        ss += "\tread_file2:\t\t\t" + read_file2.string() + "\n";
+        ss += "\tdb:\t\t\t\t" + db + "\n\n";
 
-        ss += "\tcategory_to_extract:\t" + category_to_extract + "\n";
-        ss += "\tprefix:\t" + prefix + "\n\n";
+        ss += "\tcategory_to_extract:\t\t" + category_to_extract + "\n";
+        ss += "\tprefix:\t\t\t\t" + prefix + "\n\n";
 
-        ss += "\tchunk_size:\t\t" + std::to_string(chunk_size) + "\n\n";
+        ss += "\tchunk_size:\t\t\t" + std::to_string(chunk_size) + "\n\n";
         ss += "\tlo_hi_threshold:\t\t" + std::to_string(lo_hi_threshold) + "\n";
-        ss += "\tnum_reads_to_fit:\t" + std::to_string(num_reads_to_fit) + "\n";
-        ss += "\tdist:\t\t" + dist + "\n\n";
+        ss += "\tnum_reads_to_fit:\t\t" + std::to_string(num_reads_to_fit) + "\n";
+        ss += "\tdist:\t\t\t\t" + dist + "\n\n";
 
-        ss += "\tmin_length:\t\t" + std::to_string(min_length) + "\n";
-        ss += "\tmin_quality:\t\t" + std::to_string(min_quality) + "\n";
+        ss += "\tmin_length:\t\t\t" + std::to_string(min_length) + "\n";
+        ss += "\tmin_quality:\t\t\t" + std::to_string(min_quality) + "\n";
         ss += "\tmin_compression:\t\t" + std::to_string(min_compression) + "\n";
-        ss += "\tconfidence_threshold:\t" + std::to_string(confidence_threshold) + "\n";
+        ss += "\tconfidence_threshold:\t\t" + std::to_string(confidence_threshold) + "\n";
         ss += "\thost_unique_prop_lo_threshold:\t" + std::to_string(host_unique_prop_lo_threshold) + "\n";
-        ss += "\tmin_proportion_difference:\t\t" + std::to_string(min_proportion_difference) + "\n\n";
+        ss += "\tmin_proportion_difference:\t" + std::to_string(min_proportion_difference) + "\n";
         ss += "\tmin_prob_difference:\t\t" + std::to_string(min_prob_difference) + "\n\n";
 
 
 
-        ss += "\tlog_file:\t\t" + log_file + "\n";
-        ss += "\tthreads:\t\t" + std::to_string(threads) + "\n";
-        ss += "\tverbosity:\t\t" + std::to_string(verbosity) + "\n\n";
+        ss += "\tlog_file:\t\t\t" + log_file + "\n";
+        ss += "\tthreads:\t\t\t" + std::to_string(threads) + "\n";
+        ss += "\tverbosity:\t\t\t" + std::to_string(verbosity) + "\n\n";
 
         return ss;
     }
