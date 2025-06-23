b642c96f1a965e2776a1c72e6e7b9dc75b6cc668
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2025-06-23 16:23:45 +0100

    change confidence threshold to 7 for dehost

--- include/dehost_arguments.hpp
+++ include/dehost_arguments.hpp
@@ -10,41 +10,39 @@ struct DehostArguments {
     // IO options
     std::filesystem::path read_file;
     std::filesystem::path read_file2;
-    bool is_paired { false };
+    bool is_paired{false};
     std::string db;
 
     // Output options
-    bool run_extract {false};
+    bool run_extract{false};
     std::string category_to_extract;
     std::string prefix;
     std::unordered_map<uint8_t, std::vector<std::filesystem::path>> extract_category_to_file;
 
-    uint8_t chunk_size { 100 };
+    uint8_t chunk_size{100};
 
     // Stats options
-    float lo_hi_threshold {0.15};
-    uint16_t num_reads_to_fit {5000};
+    float lo_hi_threshold{0.15};
+    uint16_t num_reads_to_fit{5000};
     std::string dist{"kde"};
 
     // thresholds for filtering
-    float min_quality { 15.0 };
-    uint32_t min_length { 140 };
-    float min_compression {0};
-    uint8_t confidence_threshold{2};
-    float confidence_probability_threshold{5};
-    float host_unique_prop_lo_threshold{ 0.05 };
-    float min_proportion_difference { 0.04 };
-    float min_prob_difference{ 0 };
-
+    float min_quality{15.0};
+    uint32_t min_length{140};
+    float min_compression{0};
+    uint8_t confidence_threshold{7};
+    float confidence_probability_threshold{0};
+    float host_unique_prop_lo_threshold{0.05};
+    float min_proportion_difference{0.04};
+    float min_prob_difference{0};
 
 
     // General options
-    std::string log_file {"charon.log"};
-    uint8_t threads { 1 };
-    uint8_t verbosity { 0 };
+    std::string log_file{"charon.log"};
+    uint8_t threads{1};
+    uint8_t verbosity{0};
 
-    std::string to_string()
-    {
+    std::string to_string() {
         std::string ss;
 
         ss += "\n\nDehost Arguments:\n\n";
@@ -68,7 +66,6 @@ struct DehostArguments {
         ss += "\thost_unique_prop_lo_threshold:\t" + std::to_string(host_unique_prop_lo_threshold) + "\n";
         ss += "\tmin_proportion_difference:\t" + std::to_string(min_proportion_difference) + "\n";
         ss += "\tmin_prob_difference:\t\t" + std::to_string(min_prob_difference) + "\n\n";
-
 
 
         ss += "\tlog_file:\t\t\t" + log_file + "\n";
